49de3e1f5b6305c38db2debf896271f46702e607
Gary Talent <gtalent2@gmail.com>
2010-10-18 00:50:48 -0500

    Documented input long for passing into Java.

--- src/blinz/input/Input.hpp
+++ src/blinz/input/Input.hpp
@@ -1,2 +1,21 @@
 #include "SDL/SDL.h"
 #include "Key.hpp"
+
+/*
+ * Event:
+ * 	bits
+ * 	00     0 = key/button status alteration, 1 = mouse wheel scroll
+ * 	32-48  x coordinate of the cursor on screen
+ * 	48-64  y coordinate of the cursor on screen
+ * Button/Key status alteration events:
+ * 	01     0 = keyboard, 1 = mouse
+ * 	02     0 = release, 1 = press
+ * 	03-31  button/key identification
+ * Mouse wheel scroll:
+ * 	01-31  scroll distance
+ */
+
+int mouseButtonPress(short button);
+int mouseButtonRelease(short button);
+int keyButtonPress(short button);
+int keyButtonRelease(short key);
