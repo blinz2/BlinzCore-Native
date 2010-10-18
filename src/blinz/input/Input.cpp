0e158f2362deec862754909a330bcbf03bdc0e17
Gary Talent <gtalent2@gmail.com>
2010-10-18 06:32:49 -0500

    Updated documentation in Input.cpp

--- src/blinz/input/Input.cpp
+++ src/blinz/input/Input.cpp
@@ -102,14 +102,16 @@ void init() {
  * Event:
  * 	bits
  * 	00     0 = key/button status alteration, 1 = mouse wheel scroll
- * 	32-48  x coordinate of the cursor on screen
- * 	48-64  y coordinate of the cursor on screen
  * Button/Key status alteration events:
  * 	01     0 = keyboard, 1 = mouse
  * 	02     0 = release, 1 = press
  * 	03-31  button/key identification
+ * 	32-48  x coordinate of the cursor on screen
+ * 	48-64  y coordinate of the cursor on screen
  * Mouse wheel scroll:
  * 	01-31  scroll distance
+ * 	32-48  x coordinate of the cursor on screen
+ * 	48-64  y coordinate of the cursor on screen
  */
 
 long mouseButtonPress(short button, long x, long y) {
